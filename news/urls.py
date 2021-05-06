3b8e0bcc8fce1c39c43a266c7e6f3dde7635ddc1
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-05-06 16:53:43 +0300

    minor

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.welcome,name = 'welcome')
+    # path('',views.welcome,name = 'welcome')
+    path('',views.past_days_news, name = 'pastNews')  
 ]
 
 urlpatterns=[
     path('',views.new_of_day, name='new_of_day')
+    
 ]
+
--- news/views.py
+++ news/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+import datetime as dt
 
 #Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to django')
+# def welcome(request):
+#     return HttpResponse('Welcome to django')
     
     
 import datetime as dt
@@ -19,6 +20,40 @@ def new_of_day(request):
         </html>
         '''
         return HttpResponse(html)
-        
-        
-        
+
+def convert_dates(dates):
+
+     day_number = datetime.date.weekday(dates)
+
+     days = ['Monday','Tuesday','Wedno','Thursday','Friday','Saturday']    
+     # returning actual day of the week
+
+     day = days[day_number]   
+     return day
+
+def new_of_day(request):
+
+     date = dt.date.today()
+
+     day = convert_dates()
+     html = f'''
+     <html>
+     <body>
+     <h1> News of the {day} {date.day}-{date.month}-{date.year}  </h1>
+     </body>
+     </html>
+     '''
+     return HttpResponse(html)
+
+def past_days_news(request,past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        day = convert_dates(date)
+        html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+        return HttpResponse(html)
