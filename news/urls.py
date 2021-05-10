07d5af9dfc3264698ae5910ad14a19527f3050fe
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-05-10 13:10:03 +0300

    minor changes

--- news/urls.py
+++ news/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    # path('',views.welcome,name = 'welcome')
-    path('',views.past_days_news, name = 'pastNews')  
-]
+   path('',views.welcome,name = 'welcome')
+#     path('',views.past_days_news, name = 'pastNews')  
+# ]
 
-urlpatterns=[
-    path('',views.new_of_day, name='new_of_day')
+# urlpatterns=[
+#     path('',views.new_of_day, name='new_of_day')
     
+# 
 ]
 
--- news/views.py
+++ news/views.py
@@ -1,59 +1,56 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-import datetime as dt
+from django.http import HttpResponse,Http404
+# import datetime as dt
+# import datetime
 
-#Create your views here.
-# def welcome(request):
-#     return HttpResponse('Welcome to django')
+def welcome(request):
+    return render(request, 'welcome.html')   
+
+# def past_days_news(request,past_date):
+#     try:
+
+#         date = dt.datetim.strptime(past_date, '%Y-%m-%d').date()
+#     except ValueError:
+#         raise Http404()
+
+
+
+# def convert_dates(dates):
+
+#      day_number = datetime.date.weekday(dates)
+
+#      days = ['Monday','Tuesday','Wedno','Thursday','Friday','Saturday','Sunday']    
+
+#      # returning actual day of the week
+#      day = days[day_number]   
+#      return day
+
+# def new_of_day(request):
+
+#      date = dt.date.today()
+
+#      day = convert_dates(date)
+#      html = f'''
+#      <html>
+#      <body>
+#      <h1> News of the {date.day}-{date.month}-{date.year} on {day}   </h1>
+#      </body>
+#      </html>
+#      '''
+#      return HttpResponse(html)
+
+# def past_days_news(request,past_date):
+#         # Converts data from the string Url
+#         date = dt.datetime.strptime(past_date,'%m-%d-%year').date()
+#         day = convert_dates(date)
+#         html = f'''
+#         <html>
+#             <body>
+#                 <h1>News for {month} {date.month}-{date.day}-{date.year}</h1>
+#             </body>
+#         </html>
+#             '''
+#         return HttpResponse(html)
+
     
-    
-import datetime as dt
-    
-def new_of_day(request):
-        date = dt.date.today()
-        html = f'''
-        <html>
-        <body>
-        <h1>{date.day}-{date.month}-{date.year}</h1>
-        </body>
-        </html>
-        '''
-        return HttpResponse(html)
-
-def convert_dates(dates):
-
-     day_number = datetime.date.weekday(dates)
-
-     days = ['Monday','Tuesday','Wedno','Thursday','Friday','Saturday']    
-     # returning actual day of the week
-
-     day = days[day_number]   
-     return day
-
-def new_of_day(request):
-
-     date = dt.date.today()
-
-     day = convert_dates()
-     html = f'''
-     <html>
-     <body>
-     <h1> News of the {day} {date.day}-{date.month}-{date.year}  </h1>
-     </body>
-     </html>
-     '''
-     return HttpResponse(html)
-
-def past_days_news(request,past_date):
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-        day = convert_dates(date)
-        html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-        return HttpResponse(html)
